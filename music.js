14cea60d09fcfa3253d6570dd50e8db62041c401
codeytimey <87842664+codeytimey@users.noreply.github.com>
2022-09-22 18:45:48 +1000

    Add files via upload

---
+++ music.js
@@ -0,0 +1,43 @@
+img="";
+status="";
+objects=[];
+
+function preload() {
+    img = loadImage('d.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects= results;
+}
+
+function draw() {
+    if (status != "") {
+        image(img, 0, 0, 640, 420);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status : Objects Detected";
+            fill("red");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
